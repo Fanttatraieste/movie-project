f162e9b84df0d78bc7150194ddf7eae96f0b7cf9
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-06-14 22:16:56 +0300

    nist ejs

---
+++ src/JavaScript/header.js
@@ -0,0 +1,27 @@
+//aici ar trb sa importez (functia de modal)
+
+module.exports = () => {
+  const search = document.querySelector('.search-bar');
+  let movieTitle;
+  search.addEventListener('input', e => {
+    movieTitle = e.target.value;
+  });
+
+  const searchBtn = document.querySelector('.search');
+  searchBtn.addEventListener('click', e => {
+    const key = require('./key').key;
+    const url = `https://api.themoviedb.org/3/search/movie?query=${movieTitle}&include_adult=false&language=en-US&page=1&api_key=${key}`;
+    let movieObj;
+    (async () => {
+      const response = await fetch(url);
+      const res = await response.json(url);
+
+      movieObj = res.results[0];
+      console.log(movieObj);
+
+      //aici ar trb sa chem modalul si sa trimit obiectul movieObj
+    })();
+
+    // console.log(movieObj);
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const key = require('./JavaScript/key').key;
 const buildBody = require('./JavaScript/body');
 const pageSelect = require('./JavaScript/numbers');
+const searchTitle = require('./JavaScript/header');
 
+searchTitle();
 pageSelect(1);
 buildBody(1, key);
